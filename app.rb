fa6431cd47b8d2738813943b2d375f77b71eef57
Robby Ranshous <rranshous@gmail.com>
2016-06-28 23:22:36 -0400

    app now takes file names w/ any char (including forward slashes), can get and delete files

--- app.rb
+++ app.rb
@@ -1,15 +1,40 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'base64'
 
 configure do
   set :data_path, File.absolute_path(ARGV.shift || './data')
 end
 
-post '/:name' do |name|
-  out_path = File.join(settings.data_path, name)
-  puts "writing to: #{out_path}"
-  FileUtils.mkdir_p File.dirname(out_path)
+post '/*' do |name|
+  encoded_name = Base64.urlsafe_encode64 name
+  file_path = File.join(settings.data_path, encoded_name)
+  puts "writing to: #{file_path}"
+  FileUtils.mkdir_p File.dirname(file_path)
   request.body.rewind
-  File.write(out_path, request.body.read)
+  File.write(file_path, request.body.read)
+  "success"
+end
+
+get '/*' do |name|
+  encoded_name = Base64.urlsafe_encode64 name
+  file_path = File.join(settings.data_path, encoded_name)
+  puts "reading #{file_path}"
+  if !File.exists? file_path
+    puts "can't read, does not exist: #{file_path}"
+    halt 404
+  end
+  send_file file_path
+end
+
+delete '/*' do |name|
+  encoded_name = Base64.urlsafe_encode64 name
+  file_path = File.join(settings.data_path, encoded_name)
+  puts "deleting: #{file_path}"
+  if !File.exists? file_path
+    puts "can't delete, does not exist: #{file_path}"
+    halt 404
+  end
+  File.unlink file_path
   "success"
 end
