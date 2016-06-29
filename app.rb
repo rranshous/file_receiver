08d0dce50d245a815a63684262c20a15ba4b716b
Robby Ranshous <rranshous@gmail.com>
2016-06-29 09:43:47 -0400

    simple web page for managing files

--- app.rb
+++ app.rb
@@ -1,9 +1,37 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'base64'
+require 'pry'
 
 configure do
   set :data_path, File.absolute_path(ARGV.shift || './data')
+end
+
+post '/form_file_upload' do
+  if params[:file] && params[:file][:filename]
+    file_name = params[:file][:filename]
+    puts "received form file: #{file_name}"
+    encoded_name = Base64.urlsafe_encode64 file_name
+    file_path = File.join(settings.data_path, encoded_name)
+    puts "writing to: #{file_path}"
+    file = params[:file][:tempfile]
+
+    File.open(file_path, 'wb') do |fh|
+      fh.write(file.read)
+    end
+    redirect to('/')
+  else
+    puts "submission did not include file"
+    halt 400, "missing file"
+  end
+end
+
+get '/' do
+  @files = Dir[File.join(settings.data_path,'*')]
+            .map{|n| File.basename n }
+            .map{|n| Base64.urlsafe_decode64(n) rescue nil }
+            .compact
+  erb :"index.html"
 end
 
 post '/*' do |name|
@@ -38,3 +66,4 @@ delete '/*' do |name|
   File.unlink file_path
   "success"
 end
+
